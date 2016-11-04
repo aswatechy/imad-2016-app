0a7dfb109383e9d33309e925f245890207a6e937
aswatechy <aswanaren@gmail.com>
2016-11-04 18:43:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ function createTemplate(data){
             var title=data.title;
             var date=data.date;
             var heading=data.heading;
-            var.content=data.content;
+            var content=data.content;
             var htmltemplate=`<html>
           <head>
               <title>
