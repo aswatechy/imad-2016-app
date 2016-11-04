7dcc5a5043fe16b6f85140b8ef5177c398731a53
aswatechy <aswanaren@gmail.com>
2016-11-04 18:57:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articlename',function(req,res){
-    var articlename=res.params.articlename;
+    var articlename=req.params.articlename;
     res.send(createTemplate(articles[articlename]));
 
 });
